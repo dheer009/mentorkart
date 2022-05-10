5a9c2386460ae34fd0d779327f977b732835c502
dheer009 <99137614+dheer009@users.noreply.github.com>
2022-05-10 23:13:53 +0530

    test file commented
    test file commented

--- Maintenant/src/test/java/com/project/maintenant/MaintenantApplicationTests.java
+++ Maintenant/src/test/java/com/project/maintenant/MaintenantApplicationTests.java
@@ -1,13 +1,13 @@
-package com.project.maintenant;
+// package com.project.maintenant;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MaintenantApplicationTests {
+// @SpringBootTest
+// class MaintenantApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     @Test
+//     void contextLoads() {
+//     }
 
-}
+// }
